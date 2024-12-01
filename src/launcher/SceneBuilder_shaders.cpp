694bbdf39dac54e3b1912c5ba609cbe67974b727
wilhelmstoritz <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>
2024-12-01 16:08:20 +0100

    ...

--- src/launcher/SceneBuilder_shaders.cpp
+++ src/launcher/SceneBuilder_shaders.cpp
@@ -59,8 +59,8 @@ void SceneBuilder::createShaders() {
         *this->m_shaderFactory->getShader("vshaderNormal"),
         *this->m_shaderFactory->getShader("fshaderPhong"));
     */
-    //this->m_shaderFactory->createShaderProgram("shaderLambertian", (this->m_resourcesPath + "03_vertexNormals.shader").c_str(), (this->m_resourcesPath + "03_fragmentLambertian.shader").c_str());
-    //this->m_shaderFactory->createShaderProgram("shaderPhong", (this->m_resourcesPath + "03_vertexNormals.shader").c_str(), (this->m_resourcesPath + "03_fragmentPhong.shader").c_str());
+    //this->m_shaderFactory->createShaderProgram("shaderLambertian", (this->m_resourcesPath + "03-normals.vert.glsl").c_str(), (this->m_resourcesPath + "03_fragmentLambertian.shader").c_str());
+    //this->m_shaderFactory->createShaderProgram("shaderPhong", (this->m_resourcesPath + "03-normals.vert.glsl").c_str(), (this->m_resourcesPath + "03_fragmentPhong.shader").c_str());
 
     /* 4th task shaders */
     this->m_shaderFactory->createShaderProgram("shaderLambertian", (this->m_resourcesPath + "04-normals.vert.glsl").c_str(), (this->m_resourcesPath + "04-lambertian.frag.glsl").c_str());
