acb69c0d6fd59afa934069fee480aabb532fa89d
wilhelmstoritz <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>
2024-12-02 00:22:15 +0100

    ...

--- src/appcore/callbacks.cpp
+++ src/appcore/callbacks.cpp
@@ -8,6 +8,20 @@ void callbackError(int t_error, const char* t_description) {
 	fputs(t_description, stderr);
 }
 
+void callbackWindowFocus(GLFWwindow* t_window, int t_focused) {
+	printf("<callback> window focus\n");
+}
+
+void callbackWindowIconify(GLFWwindow* t_window, int t_iconified) {
+	printf("<callback> window iconify\n");
+}
+
+void callbackFramebufferSize(GLFWwindow* t_window, int t_width, int t_height) {
+	printf("<callback> framebuffer size : width %d, height %d\n", t_width, t_height);
+
+	glViewport(0, 0, t_width, t_height);
+}
+
 void callbackKey(GLFWwindow* t_window, int t_key, int t_scancode, int t_action, int t_mods) {
 	printf("<callback> key : key %d, scancode %d, action %d, mods %d\n", t_key, t_scancode, t_action, t_mods);
 
@@ -22,18 +36,4 @@ void callbackButton(GLFWwindow* t_window, int t_button, int t_action, int t_mode
 
 void callbackCursor(GLFWwindow* t_window, double t_x, double t_y) {
 	printf("<callback> cursor\n");
-}
-
-void callbackWindowFocus(GLFWwindow* t_window, int t_focused) {
-	printf("<callback> window focus\n");
-}
-
-void callbackWindowIconify(GLFWwindow* t_window, int t_iconified) {
-	printf("<callback> window iconify\n");
-}
-
-void callbackFramebufferSize(GLFWwindow* t_window, int t_width, int t_height) {
-	printf("<callback> framebuffer size : width %d, height %d\n", t_width, t_height);
-
-	glViewport(0, 0, t_width, t_height);
 }
--- src/appcore/callbacks.h
+++ src/appcore/callbacks.h
@@ -5,10 +5,10 @@
 
 void callbackError(int t_error, const char* t_description);
 
-void callbackKey(GLFWwindow* t_window, int t_key, int t_scancode, int t_action, int t_mods);
-void callbackButton(GLFWwindow* t_window, int t_button, int t_action, int t_mode);
-void callbackCursor(GLFWwindow* t_window, double t_x, double t_y);
-
 void callbackWindowFocus(GLFWwindow* t_window, int t_focused);
 void callbackWindowIconify(GLFWwindow* t_window, int t_iconified);
 void callbackFramebufferSize(GLFWwindow* t_window, int t_width, int t_height);
+
+void callbackKey(GLFWwindow* t_window, int t_key, int t_scancode, int t_action, int t_mods);
+void callbackButton(GLFWwindow* t_window, int t_button, int t_action, int t_mode);
+void callbackCursor(GLFWwindow* t_window, double t_x, double t_y);
