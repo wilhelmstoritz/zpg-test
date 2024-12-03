8fe57ead9fc0cd2c39326933c06120e969e3471e
wilhelmstoritz <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>
2024-12-03 23:12:17 +0100

    example 06b bezier added

---
+++ src/examples/06b-bezier/bezier.cpp
@@ -0,0 +1,203 @@
+ //Include GLEW
+#include <GL/glew.h>
+//Include GLFW
+#include <GLFW/glfw3.h> 
+
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
+#include <glm/gtx/string_cast.hpp>
+
+#include <SOIL.h>
+
+#include<assimp/Importer.hpp>// C++ importerinterface
+#include<assimp/scene.h>// aiSceneoutputdata structure
+#include<assimp/postprocess.h>// Post processingflags
+
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>   // std::setprecision, std::setw
+#include <vector>
+
+const char* vertex_shader =
+"#version 330\n"
+"layout(location = 0) in vec3 vp;"
+"layout(location = 1) in vec3 vc;"
+"layout(location = 2) in vec2 uv; "
+"uniform mat4 modelMatrix;"
+"out vec2 uvc;"
+    ""
+"void main () {"
+"    gl_Position = modelMatrix * vec4 (vp, 1.0);"
+"    uvc=uv;"
+"}";
+
+const char* fragment_shader =
+"#version 330\n"
+"out vec4 frag_colour;"
+"uniform sampler2D textureUnitID;"
+"in vec2 uvc;"
+"void main () {"
+"     frag_colour = texture(textureUnitID, uvc);"
+"}";
+
+
+static void error_callback(int error, const char* description) { 
+    fputs(description, stderr); 
+}
+
+using namespace std;
+
+int main(void)
+{
+
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        exit(EXIT_FAILURE);
+    }
+
+    window = glfwCreateWindow(800, 600, "ZPG", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    float ratio = width / (float)height;
+    glViewport(0, 0, width, height);
+    
+    int count=0;
+    Assimp::Importer importer;
+    unsigned int importOptions = aiProcess_Triangulate
+        | aiProcess_OptimizeMeshes              // sloučení malých plošek
+        | aiProcess_JoinIdenticalVertices       // NUTNÉ jinak hodně duplikuje
+        | aiProcess_Triangulate                 // prevod vsech ploch na trojuhelniky
+        | aiProcess_CalcTangentSpace;           // vypocet tangenty, nutny pro spravne pouziti normalove mapy
+    const aiScene* scene = importer.ReadFile("tree.obj", importOptions);
+    vector<float> data;
+    if (scene) {
+        aiMesh* mesh = scene->mMeshes[0];
+        count = mesh->mNumFaces * 3;
+        for (unsigned int i = 0; i < mesh->mNumFaces; i++){
+            for (unsigned int j = 0; j < 3; j++)
+            {
+                data.push_back(mesh->mVertices[mesh->mFaces[i].mIndices[j]].x);
+                data.push_back(mesh->mVertices[mesh->mFaces[i].mIndices[j]].y);
+                data.push_back(mesh->mVertices[mesh->mFaces[i].mIndices[j]].z);
+                data.push_back(mesh->mNormals[mesh->mFaces[i].mIndices[j]].x);
+                data.push_back(mesh->mNormals[mesh->mFaces[i].mIndices[j]].y);
+                data.push_back(mesh->mNormals[mesh->mFaces[i].mIndices[j]].z);
+                data.push_back(mesh->mTextureCoords[0][mesh->mFaces[i].mIndices[j]].x);
+                data.push_back(mesh->mTextureCoords[0][mesh->mFaces[i].mIndices[j]].y);
+            }
+        }
+    }
+    //Vertex Array Object (VAO)
+    GLuint VBO = 0;
+    glGenBuffers(1, &VBO); // generate the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, data.size()*sizeof(float), &data[0], GL_STATIC_DRAW);
+    
+    GLuint VAO = 0;
+    glGenVertexArrays(1, &VAO); //generate the VAO
+    glBindVertexArray(VAO); //bind the VAO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    //enable vertex attributes
+    glEnableVertexAttribArray(0); 
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (GLvoid*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (GLvoid*)(sizeof(float) * 3));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (GLvoid*)(sizeof(float) * 6));
+    
+
+    //create and compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+    glCompileShader(vertexShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+    glCompileShader(fragmentShader);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vertexShader);
+    glLinkProgram(shaderProgram);
+    GLint status;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
+    
+    glActiveTexture(GL_TEXTURE0);
+    GLuint image = SOIL_load_OGL_texture("tree.png", SOIL_LOAD_RGBA, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    if (image == NULL) {
+        std::cout << "An error occurred while loading image." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    glBindTexture(GL_TEXTURE_2D, image);
+
+    glm::mat4 M = glm::mat4(1.0f);
+    glm::mat4 A = glm::mat4(glm::vec4(-1.0, 3.0, -3.0, 1.0),
+        glm::vec4(3.0, -6.0, 3.0, 0),
+        glm::vec4(-3.0, 3.0, 0, 0),
+        glm::vec4(1, 0, 0, 0));
+    std::cout << glm::to_string(A) << std::endl;
+    glm::mat4x3 B = glm::mat4x3(glm::vec3(-1, 0, 0),
+        glm::vec3(0, 1, 0),
+        glm::vec3(0, -1, 0),
+        glm::vec3(1, 0, 0));
+
+    
+    float t = 0.5f;
+    float delta = 0.01;
+
+    glEnable(GL_DEPTH_TEST);
+    while (!glfwWindowShouldClose(window)) {
+        glm::vec4 parameters = glm::vec4(t * t * t, t * t, t, 1.0f);
+        glm::vec3 p = parameters * A * glm::transpose(B);
+        // clear color and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+
+        M = glm::translate(glm::mat4(1.0f), p);
+        M = glm::scale(M, glm::vec3(0.015));
+
+        GLint idModelTransform = glGetUniformLocation(shaderProgram, "modelMatrix");
+        glUniformMatrix4fv(idModelTransform, 1, GL_FALSE, &M[0][0]);
+
+        // draw triangles
+        glDrawArrays(GL_TRIANGLES, 0, count); //mode,first,count
+        // update other events like input handling
+       
+        if (t >= 1.0f || t <= 0.0f) delta *= -1;
+        t += delta;
+	glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
