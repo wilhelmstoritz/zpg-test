e6658e96d9909da8585b7efddf64d815ec8a930d
wilhelmstoritz <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>
2024-12-10 00:31:51 +0100

    ...

--- src/gloo/LightFactory.cpp
+++ src/gloo/LightFactory.cpp
@@ -1,4 +1,5 @@
 #include "LightFactory.h"
+#include "LightFlashlight.h"
 
 // --- public ------------------------------------------------------------------
 std::unique_ptr<Light> LightFactory::createLight(
@@ -9,4 +10,16 @@ std::unique_ptr<Light> LightFactory::createLight(
 	const float t_spotCutoff)
 {
 	return std::make_unique<Light>(t_name, t_type, t_position, t_direction, t_spotCutoff);
+}
+
+std::unique_ptr<Light> LightFactory::createFlashlight(
+	const std::string& t_name,
+	const glm::vec3& t_position,
+	const glm::vec3& t_direction)
+{
+	return std::make_unique<LightFlashlight>(t_name, t_position, t_direction);
+}
+
+std::unique_ptr<Light> LightFactory::createFlashlight(const std::string& t_name, Camera* t_camera) {
+	return std::make_unique<LightFlashlight>(t_name, t_camera);
 }
--- src/gloo/LightFactory.h
+++ src/gloo/LightFactory.h
@@ -20,4 +20,10 @@ public:
 		const glm::vec3& t_position,
 		const glm::vec3& t_direction = glm::vec3(0.f, 0.f, -1.f),
 		const float t_spotCutoff = 0.f);
+
+	std::unique_ptr<Light> createFlashlight(
+		const std::string& t_name,
+		const glm::vec3& t_position = glm::vec3(0.f),
+		const glm::vec3& t_direction = glm::vec3(0.f, 0.f, -1.f));
+	std::unique_ptr<Light> createFlashlight(const std::string& t_name, Camera* t_camera);
 };
