db166c0884b831440c3e8f0844e5f6d546e437c6
wilhelmstoritz <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>
2024-12-29 00:39:32 +0100

    ...

--- src/resources/reslib/ModelLetters.h
+++ src/resources/reslib/ModelLetters.h
@@ -1,11 +1,7 @@
 #pragma once
 
 // include GLM
-#include <glm/vec3.hpp> // glm::vec3
-#include <glm/vec4.hpp> // glm::vec4
-#include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
-#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
+#include <glm/vec2.hpp> // glm::vec2
 
 // standard C++ libraries
 #include <mutex>
