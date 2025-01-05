a18979856ab5ce690d9b846e2dba850daabbb1e8
wilhelmstoritz <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>
2025-01-05 02:21:27 +0100

    ...

--- src/launcher/launcher.cpp
+++ src/launcher/launcher.cpp
@@ -1,23 +1,6 @@
-﻿/*
-// GLEW
-#include <GL/glew.h>
-
-// GLFW
-#include <GLFW/glfw3.h>
-
-// GLM
-#include <glm/vec3.hpp> // glm::vec3
-#include <glm/vec4.hpp> // glm::vec4
-#include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
-#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
-
-// standard C++ libraries
-#include <stdlib.h>
-#include <stdio.h>
-*/
-
+﻿#include "Application.h"
 #include "SceneBuilder.h"
+
 #include "SceneBuilderPluginEmptyScene.h"
 #include "SceneBuilderPluginMenu.h"
 #include "SceneBuilderPluginTest.h"
@@ -30,7 +13,6 @@
 #include "SceneBuilderPlugin05b.h"
 #include "SceneBuilderPlugin05c.h"
 #include "SceneBuilderPlugin06.h"
-#include "Application.h"
 
 int main(void) {
 	SceneBuilder* builder = SceneBuilder::getInstance();
