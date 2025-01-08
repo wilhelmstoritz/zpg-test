745f994f323927c5476b4cab1b39aa246573e922
wilhelmstoritz <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>
2025-01-08 10:39:35 +0100

    ...

--- src/launcher/SceneBuilder.h
+++ src/launcher/SceneBuilder.h
@@ -14,7 +14,7 @@ public:
 	~SceneBuilder() = default;
 
 	template <typename T>
-	Scene* createScene(const std::string& t_name, SceneBuilderPlugin* t_sceneBuilderPlugin);
+	T* createScene(const std::string& t_name, SceneBuilderPlugin* t_sceneBuilderPlugin);
 
 private:
 	// - - static class properties - - - - - - - - - - - - - - - - - - - - - - - -
@@ -33,23 +33,4 @@ private:
 };
 
 // === template implementation =================================================
-// --- public ------------------------------------------------------------------
-template <typename T>
-Scene* SceneBuilder::createScene(const std::string& t_name, SceneBuilderPlugin* t_sceneBuilderPlugin) {
-	static_assert(std::is_base_of<Scene, T>::value, "T must be derived from Scene"); // check if T is derived from Scene; if not, the compilation will fail
-
-	// new empty scene
-	Scene* scene = new T(t_name, new Camera("default",
-		glm::vec3(0.f, 0.f, 1.f),    // eye
-		glm::vec3(0.f, 0.f, -1.f))); // direction
-
-	// fill the scene and bring it to life
-	t_sceneBuilderPlugin->createContext(scene);
-	scene->setAllCameras();
-	scene->setAllLights();
-
-	// cleanup
-	delete t_sceneBuilderPlugin;
-
-	return scene;
-}
+#include "SceneBuilder.tpp"
---
+++ src/launcher/SceneBuilder.tpp
@@ -0,0 +1,23 @@
+#include "SceneBuilder.h"
+
+// === template implementation =================================================
+// --- public ------------------------------------------------------------------
+template <typename T>
+T* SceneBuilder::createScene(const std::string& t_name, SceneBuilderPlugin* t_sceneBuilderPlugin) {
+	static_assert(std::is_base_of<Scene, T>::value, "T must be derived from Scene"); // check if T is derived from Scene; if not, the compilation will fail
+
+	// new empty scene
+	T* scene = new T(t_name, new Camera("default",
+		glm::vec3(0.f, 0.f, 1.f),    // eye
+		glm::vec3(0.f, 0.f, -1.f))); // direction
+
+	// fill the scene and bring it to life
+	t_sceneBuilderPlugin->createContext(scene);
+	scene->setAllCameras();
+	scene->setAllLights();
+
+	// cleanup
+	delete t_sceneBuilderPlugin;
+
+	return scene;
+}
