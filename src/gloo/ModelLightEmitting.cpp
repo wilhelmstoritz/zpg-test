f270301f1980419b4f5b89a18ab5b34066c59932
wilhelmstoritz <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>
2025-01-08 08:03:50 +0100

    ...

--- src/gloo/ModelLightEmitting.cpp
+++ src/gloo/ModelLightEmitting.cpp
@@ -1,19 +1,11 @@
 #include "ModelLightEmitting.h"
 
 // --- public ------------------------------------------------------------------
-/*ModelLightEmitting::ModelLightEmitting(ShaderProgram* t_shaderProgram, VAO* t_vao, GLint t_first, GLsizei t_count)
-	: Model(t_shaderProgram, t_vao, t_first, t_count) {
-	// color, intensity and transition time; default values
-	//this->m_diffuseColor = this->generateRandomColor();
-	//this->m_specularColor = glm::vec3(.6f);
-	//this->m_specularColor = this->m_diffuseColor;
-	
-
-	//this->m_kDiffuse = AppUtils::getInstance()->randomNumber(RND_DIFFUSE_MIN, RND_DIFFUSE_MAX);
-}*/
-
 ModelLightEmitting::ModelLightEmitting(const std::string& t_name, ShaderProgram* t_shaderProgram, VAO* t_vao, IBO* t_ibo, GLint t_first, GLsizei t_count)
 	: Model(t_name, t_shaderProgram, t_vao, t_ibo, t_first, t_count) {
+	// color and reflection coefficient
+	this->m_specularColor = glm::vec3(1.f); // white
+	this->m_kSpecular = 1.f; // maximum reflection
 }
 
 ModelLightEmitting::ModelLightEmitting(const std::string& t_name, ShaderProgram* t_shaderProgram, VAO* t_vao, GLint t_first, GLsizei t_count)
@@ -24,6 +16,12 @@ ModelLightEmitting::ModelLightEmitting(ShaderProgram* t_shaderProgram, VAO* t_va
 
 ModelLightEmitting::ModelLightEmitting(ShaderProgram* t_shaderProgram, VAO* t_vao, GLint t_first, GLsizei t_count)
 	: ModelLightEmitting("@!#?@!", t_shaderProgram, t_vao, nullptr, t_first, t_count) { }
+
+glm::vec3 ModelLightEmitting::getSpecularColor() { return this->m_specularColor; }
+float ModelLightEmitting::getKSpecular() { return this->m_kSpecular; }
+
+void ModelLightEmitting::setSpecularColor(const glm::vec3& t_specularColor) { this->m_specularColor = t_specularColor; }
+void ModelLightEmitting::setKSpecular(float t_kSpecular) { this->m_kSpecular = t_kSpecular; }
 
 bool ModelLightEmitting::animate() { return false; } // default implementation; no animation
 
--- src/gloo/ModelLightEmitting.h
+++ src/gloo/ModelLightEmitting.h
@@ -10,8 +10,17 @@ public:
 	ModelLightEmitting(ShaderProgram* t_shaderProgram, VAO* t_vao, IBO* t_ibo, GLint t_first, GLsizei t_count);
 	ModelLightEmitting(ShaderProgram* t_shaderProgram, VAO* t_vao,             GLint t_first, GLsizei t_count);
 
+	glm::vec3 getSpecularColor();
+	float getKSpecular();
+
+	void setSpecularColor(const glm::vec3& t_specularColor);
+	void setKSpecular(float t_kSpecular);
+
 	virtual bool animate() override;
 
 protected:
+	glm::vec3 m_specularColor;
+	float m_kSpecular; // specular reflection coefficient
+
 	virtual void postUpdate() override;
 };
