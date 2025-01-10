b9411b7accd781a667e55cbaa7cf10f959963e2e
wilhelmstoritz <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>
2025-01-10 16:59:16 +0100

    ...

--- src/launcher/Application.cpp
+++ src/launcher/Application.cpp
@@ -96,8 +96,8 @@ void Application::run() {
 	this->setScene(this->m_scene);
 
 	// gl settings
-	glfwSetWindowPos(this->m_window, this->m_windowXpos, this->m_windowYpos);
-	glfwSetWindowSize(this->m_window, Config::WINDOW_SIZE.x, Config::WINDOW_SIZE.y);
+	//glfwSetWindowPos(this->m_window, this->m_windowXpos, this->m_windowYpos);
+	//glfwSetWindowSize(this->m_window, Config::WINDOW_SIZE.x, Config::WINDOW_SIZE.y);
 	//this->updateViewport(); // framebuffer resize callback does the trick; no need to update the viewport manually
 
 	glEnable(GL_DEPTH_TEST); // z-buffer; do depth comparisons and update the depth buffer
