44bc7a6246ab45e10996e9000fea289a12867d3b
wilhelmstoritz <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>
2025-01-11 17:32:26 +0100

    ...

--- src/launcher/SceneBuilderPluginTest.cpp
+++ src/launcher/SceneBuilderPluginTest.cpp
@@ -27,7 +27,8 @@
 void SceneBuilderPluginTest::setSceneProperties(Scene* t_scene) {
     t_scene->setTitle(this->m_windowTitle
         + Config::WINDOW_TITLE_MOVE
-        + Config::WINDOW_TITLE_HELP_FLASHLIGHT);
+        + Config::WINDOW_TITLE_HELP_FLASHLIGHT
+        + Config::WINDOW_TITLE_HELP_FIREBALL);
 }
 
 // --- protected ---------------------------------------------------------------
