0f308718371326f67fd9dc624252c204335430fd
wilhelmstoritz <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>
2025-01-11 22:47:53 +0100

    ...

--- src/gloo/SceneFireball.cpp
+++ src/gloo/SceneFireball.cpp
@@ -100,12 +100,12 @@ void SceneFireball::throwFireball() {
 	glm::vec3 bStart = fireball->getTransformation()->getTranslateStep()->getTranslation(); // start point at the current position of the fireball
 	glm::vec3 bEnd = bStart + range * directionXZ; // end point in the direction of the XZ plane projection
 
-	std::vector<glm::vec3> controlPoints = { (bStart + bEnd) / 2.f + glm::vec3(0.f, height, 0.f) }; // control point above the middle of the start point and the end point in the direction of the XZ plane projection
+	glm::vec3 bControl = (bStart + bEnd) / 2.f + glm::vec3(0.f, height, 0.f); // control point above the middle of the start point and the end point in the direction of the XZ plane projection
 
 	bEnd.y = this->m_min.y + .03f; // end point slightly above ground
 
 	fireball->getTransformation()->updateTranslateStep(
 		std::make_shared<TransformationAnimationBezierCurve>(
-			std::vector<glm::vec3>{ bStart, bEnd },
+			std::vector<glm::vec3>{ bStart, bControl, bEnd },
 			power * 3.f)); // 3 times longer duration; power = seconds
 }
