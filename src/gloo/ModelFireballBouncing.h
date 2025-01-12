b085195d396aecdb6dc186ccdb0c57cd5eda2fd8
wilhelmstoritz <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>
2025-01-12 07:11:45 +0100

    ...

---
+++ src/gloo/ModelFireballBouncing.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ModelFireball.h"
+
+class ModelFireballBouncing : public ModelFireball {
+public:
+	ModelFireballBouncing(const std::string& t_name, ShaderProgram* t_shaderProgram, VAO* t_vao, IBO* t_ibo, GLint t_first, GLsizei t_count);
+	ModelFireballBouncing(const std::string& t_name, ShaderProgram* t_shaderProgram, VAO* t_vao,             GLint t_first, GLsizei t_count);
+	ModelFireballBouncing(ShaderProgram* t_shaderProgram, VAO* t_vao, IBO* t_ibo, GLint t_first, GLsizei t_count);
+	ModelFireballBouncing(ShaderProgram* t_shaderProgram, VAO* t_vao,             GLint t_first, GLsizei t_count);
+
+	virtual void animateContinuous() override;
+};
