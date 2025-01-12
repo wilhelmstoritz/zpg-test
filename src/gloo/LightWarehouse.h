5ec59a1e04c58466564f6043a3ac845fe7025405
wilhelmstoritz <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>
2025-01-12 11:52:24 +0100

    ...

--- src/gloo/LightWarehouse.h
+++ src/gloo/LightWarehouse.h
@@ -25,7 +25,7 @@ public:
 	Light* createLight(
 		const std::string& t_name,
 		const int t_type,
-		const glm::vec3& t_position,
+		const glm::vec3& t_position = glm::vec3(0.f),
 		const glm::vec3& t_direction = glm::vec3(0.f, 0.f, -1.f),
 		const float t_spotCutoff = 0.f);
 
