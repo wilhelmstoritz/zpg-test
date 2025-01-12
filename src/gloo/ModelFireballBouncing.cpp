1293ee4d8f1fc7354f65b9d18cbd8cf85fbc7b02
wilhelmstoritz <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>
2025-01-12 07:18:06 +0100

    ...

---
+++ src/gloo/ModelFireballBouncing.cpp
@@ -0,0 +1,29 @@
+#include "ModelFireballBouncing.h"
+
+#define RND_DIFFUSE_MIN .5f
+#define RND_DIFFUSE_MAX 1.f
+#define RND_SPECULAR_MIN .5f
+#define RND_SPECULAR_MAX 1.f
+
+#define RND_TIME_DIFFUSE_MIN .3f
+#define RND_TIME_DIFFUSE_MAX .6f
+#define RND_TIME_SPECULAR_MIN .03f
+#define RND_TIME_SPECULAR_MAX .1f
+
+// --- public ------------------------------------------------------------------
+ModelFireballBouncing::ModelFireballBouncing(const std::string& t_name, ShaderProgram* t_shaderProgram, VAO* t_vao, IBO* t_ibo, GLint t_first, GLsizei t_count)
+	: ModelFireball(t_name, t_shaderProgram, t_vao, t_ibo, t_first, t_count) {
+}
+
+ModelFireballBouncing::ModelFireballBouncing(const std::string& t_name, ShaderProgram* t_shaderProgram, VAO* t_vao, GLint t_first, GLsizei t_count)
+	: ModelFireballBouncing(t_name, t_shaderProgram, t_vao, nullptr, t_first, t_count) { }
+
+ModelFireballBouncing::ModelFireballBouncing(ShaderProgram* t_shaderProgram, VAO* t_vao, IBO* t_ibo, GLint t_first, GLsizei t_count)
+	: ModelFireballBouncing("@!#?@!", t_shaderProgram, t_vao, t_ibo, t_first, t_count) { }
+
+ModelFireballBouncing::ModelFireballBouncing(ShaderProgram* t_shaderProgram, VAO* t_vao, GLint t_first, GLsizei t_count)
+	: ModelFireballBouncing("@!#?@!", t_shaderProgram, t_vao, nullptr, t_first, t_count) { }
+
+void ModelFireball::animateContinuous() {
+	this->setState(fireballStateE::STATE_IDLE); // default implementation; the fireball stops when it reaches the destination; no continuous animation
+}
