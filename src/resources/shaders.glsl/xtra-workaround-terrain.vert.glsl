22baebfa37af395e9cea1982b879afd8e35427f6
wilhelmstoritz <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>
2025-01-13 22:32:39 +0100

    ...

---
+++ src/resources/shaders.glsl/xtra-workaround-terrain.vert.glsl
@@ -0,0 +1,20 @@
+/* 5th task; vertex shader; cubemap texture (from position) */
+//#version 460 core // GLSL latest version
+#version 430 core // latest version supported by VMware SVGA 3D virtual graphics driver
+
+uniform mat4 modelMatrix, viewMatrix, projectionMatrix;
+
+// input variables; from the vertex buffer, must match VBO attributes
+layout(location = 0) in vec3 position;
+
+// output variables; will be passed to the fragment shader
+out vec2 textureCoord;
+
+void main() {
+	minCoord = -100.f;
+	maxCoord =  100.f;
+	textureCoord = (position.xz - minCoord) / (maxCoord - minCoord);
+
+	// vertex position in the clip space
+	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.f);
+}
