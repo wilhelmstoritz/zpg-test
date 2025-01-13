ead058b927c7e1c0a74bf89b1c5789bf4f4ffc43
wilhelmstoritz <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>
2025-01-14 00:19:19 +0100

    ...

--- src/gloo/SceneFireball.cpp
+++ src/gloo/SceneFireball.cpp
@@ -120,9 +120,10 @@ std::vector<std::vector<glm::vec3>> SceneFireball::zigzagCurve(const std::vector
 	curveSegment.push_back(t_bezierCurve[0]); // the starting point remains the same as on the original bezier curve
 
 	// sampling the original bezier curve
+	size_t numSegments = Config::ENVIRONMENT_FIREBALL_PATH_COMPLEXITY;
 	size_t bezierCurveDegree = 2; // bezier of 2nd degree (quadratic)
 
-	size_t numSamples = Config::ENVIRONMENT_FIREBALL_PATH_COMPLEXITY * bezierCurveDegree; // degree + 1 points per segment; but! start and end points shared between neighbors
+	size_t numSamples = numSegments * bezierCurveDegree; // degree + 1 points per segment; but! start and end points shared between neighbors
 	for (size_t i = 1; i < numSamples; ++i) { // omit the first point (it is already added) and the last point (will be added later)
 		float t = static_cast<float>(i) / numSamples;
 		glm::vec3 point = AppMath::getInstance()->bezierPoint(t_bezierCurve, t); // point on the original bezier curve
