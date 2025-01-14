d25a99e4212a285df27379b7e7286c5b609cdcd2
wilhelmstoritz <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>
2025-01-14 03:55:13 +0100

    ...

--- src/appcore/Config.cpp
+++ src/appcore/Config.cpp
@@ -10,7 +10,7 @@ const std::string Config::SYSTEM_RESOURCES_RELPATH_MODELS   = "models.obj/";
 const std::string Config::SYSTEM_RESOURCES_RELPATH_TEXTURES = "textures/";
 const std::string Config::SYSTEM_RESOURCES_RELPATH_FONTS    = "fonts/";
 
-const std::string Config::SYSTEM_SPLASH_IMAGE = "splash/splash1.jpg";
+const std::string Config::SYSTEM_SPLASH_IMAGE = "my/splash/splash1.jpg";
 const bool        Config::SYSTEM_SPLASH_RANDOM       = true; // if false, the splash image is shown; if true, a random splash image is shown (must differ by number)
 const size_t      Config::SYSTEM_SPLASH_RANDOM_COUNT = 5;
 const bool        Config::SYSTEM_SPLASH_SHOW         = true;
