a70f39c2032aaa098f27e765d521186afc2f88af
wilhelmstoritz <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>
2025-01-15 00:46:21 +0100

    ...

--- src/launcher/Application.cpp
+++ src/launcher/Application.cpp
@@ -226,7 +226,7 @@ Application::Application() {
 	this->m_window = this->initWindow(); // application window
 
 	this->versionInfo(); // version info; console output
-	printf("[application] loading...\n");
+	printf("[application] loading...");
 
 	// controler, renderer; scene(s) will be added later
 	this->m_controller = new Controller(this->m_window);
