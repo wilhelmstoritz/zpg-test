977b00474c12bf342572211ebcde4db28c969132
wilhelmstoritz <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>
2025-01-16 08:51:14 +0100

    ...

--- src/resources/reslib/resModels.cpp
+++ src/resources/reslib/resModels.cpp
@@ -3,14 +3,14 @@
 /*** 1st task ***/
 // triangle
 const std::vector<float> MODEL_TRIANGLE = { // 3 vertices (3 floats per vertex; 1 triangle)
-	0.f,   .5f, 0.f,
+	 .0f,  .5f, 0.f,
 	 .5f, -.5f, 0.f,
 	-.5f, -.5f, 0.f
 };
 
 const std::vector<float> MODEL_TRIANGLE_COLORDATA = { // 3 vertices (3+3 floats per vertex; 1 triangle)
 	// coords          // color
-	0.f,   .5f, 0.f,   1.f, 0.f, 0.f, // red
+	 .0f,  .5f, 0.f,   1.f, 0.f, 0.f, // red
 	 .5f, -.5f, 0.f,   0.f, 1.f, 0.f, // green
 	-.5f, -.5f, 0.f,   0.f, 0.f, 1.f  // blue
 };
